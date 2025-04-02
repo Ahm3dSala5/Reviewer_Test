1cc8d4d363b84493bd985afa1e3a5f3ade26270c
Ahmed Salah <yarapharpyaa55@gmail.com>
2025-04-02 14:45:43 +0200

    Add_AttributesPage_Scripts

--- Reviewer_Test/30_Reviewer.Configuration.Attributes.Test.cs
+++ Reviewer_Test/30_Reviewer.Configuration.Attributes.Test.cs
@@ -43,58 +43,208 @@ namespace Reviewer_Test
         }
 
         [Test]
-        public void ReviewerConfigurationAttributes_WhenClickOnConfiguration_MustOpenDropdownlist()
+        public void AttributesPage_ConfigurationOptionTest()
         {
-            var confiOption = driver.FindElement
-                (By.XPath("//*[@id=\"m_ver_menu\"]/ul/li[3]/a/span/span"));
-            confiOption.Click();
+            var ConfigurationOption = driver.FindElement
+                (By.XPath("//*[@id=\"m_ver_menu\"]/ul/li[3]/a"));
+
+            Assert.IsTrue(ConfigurationOption.Enabled);
+            Assert.IsTrue(ConfigurationOption.Displayed);
+            Assert.AreEqual(ConfigurationOption.Text, "Configuration");
+            Assert.AreEqual(ConfigurationOption.GetAttribute("custom-data"), "Configuration");
+            Assert.AreEqual(ConfigurationOption.GetAttribute("aria-expanded"), "false");
         }
 
         [Test]
-        public void ReviewerConfigurationAttributes_WhenClickOnAttributesOption_MustOpenAttributesPage()
+        public void AtttributesPage_AttributeOptionTest()
         {
-            // to click on configuration btn 
-            ReviewerConfigurationAttributes_WhenClickOnConfiguration_MustOpenDropdownlist();
+            var ConfigurationOption = driver.FindElement
+               (By.XPath("//*[@id=\"m_ver_menu\"]/ul/li[3]/a"));
+            ConfigurationOption.Click();
 
-            var attributesBtn = driver.FindElement
-                (By.XPath("//*[@id=\"m_ver_menu\"]/ul/li[3]/nav/ul/li/a/span/span"));
-            attributesBtn.Click();
+            var AttributesOption = driver.FindElement(By.XPath("//*[@id=\"m_ver_menu\"]/ul/li[3]/nav/ul/li/a"));
+            Assert.IsTrue(AttributesOption.Enabled);
+            Assert.IsTrue(AttributesOption.Displayed);
+            Assert.AreEqual(AttributesOption.Text, "Attributes");
+            Assert.AreEqual(AttributesOption.GetAttribute("custom-data"), "Attributes");
+            Assert.AreEqual(AttributesOption.GetAttribute("target"), "_self");
+            Assert.AreEqual(AttributesOption.GetAttribute("aria-expanded"), "false");
+            Assert.AreEqual(AttributesOption.GetAttribute("href"), "http://ec2-34-226-24-71.compute-1.amazonaws.com/App/AssetsConfigaration");
         }
 
         [Test]
-        public void ReviewerConfigurationAttributes_WhenSelectAssetClass_MustFilterAllAssetsByClass()
+        public void AttributesPage_OpenPage()
         {
-            // to click on configuration btn 
-            ReviewerConfigurationAttributes_WhenClickOnAttributesOption_MustOpenAttributesPage();
+            var ConfigurationOption = driver.FindElement
+               (By.XPath("//*[@id=\"m_ver_menu\"]/ul/li[3]/a"));
+            ConfigurationOption.Click();
 
-            var assetClass = driver.FindElement(By.Id("AssetClassIdChange"));
-            var selectedAssetClass = new SelectElement(assetClass);
+            var AttibutesOption = driver.FindElement
+                (By.XPath("//*[@id=\"m_ver_menu\"]/ul/li[3]/nav/ul/li/a"));
+            AttibutesOption.Click();
+        }
+
+        [Test]
+        public void AttributesPage_HiHostOperatorTest()
+        {
+            // to open Attributes page 
+            AttributesPage_OpenPage();
+
+            var hiHostOperator = driver.FindElement
+                (By.XPath("//*[@id=\"m_header_topbar\"]/div/ul/li[4]/a/span[1]"));
+            Assert.AreEqual(hiHostOperator.Text, "HI,");
+            Assert.True(hiHostOperator.Displayed);
+            Assert.True(hiHostOperator.Enabled);
+
+            var username = driver.FindElement(By.Id("UserName"));
+            Assert.True(username.Displayed);
+            Assert.True(username.Enabled);
+        }
+
+        [Test]
+        public void AttributesPage_LogoutBtn()
+        {
+            // to open Attributes page 
+            AttributesPage_OpenPage();
+
+            var hiHostOperator = driver.FindElement
+                (By.XPath("//*[@id=\"m_header_topbar\"]/div/ul/li[4]/a/span[1]"));
+            hiHostOperator.Click();
+
+            var logoutBtn = driver.FindElement
+                (By.XPath("//*[@id=\"m_header_topbar\"]/div/ul/li[4]/div/div/div/div/ul/li[4]/a"));
+            Assert.IsTrue(logoutBtn.Enabled);
+            Assert.IsTrue(logoutBtn.Displayed);
+            Assert.AreEqual(logoutBtn.Text, "Logout");
+
+            var UrlBeforeClickOnLogout = driver.Url;
+            logoutBtn.Click();
+            var UrlAfterClickOnLogout = driver.Url;
+            Assert.AreNotEqual(UrlBeforeClickOnLogout, UrlAfterClickOnLogout);
+        }
+
+        [Test]
+        public void AttributesPage_ModalTest()
+        {
+            // to open Attributes page 
+            AttributesPage_OpenPage();
+
+            var modalTitle = driver.FindElement(By.XPath("//*[@id=\"demo\"]"));
+            Assert.IsFalse(modalTitle.Displayed);
+            Assert.IsTrue(modalTitle.Enabled);
+
+            var selectedTenant = driver.FindElement(By.Id("TenantDropDownChange"));
+            var selectedTenantValue = new SelectElement(selectedTenant);
+            selectedTenantValue.SelectByIndex(0);
+
+            var closeBtn = driver.FindElement(By.Id("close"));
+            Assert.AreEqual(closeBtn.GetAttribute("type"), "button");
+            Assert.IsFalse(closeBtn.Displayed);
+            Assert.IsTrue(closeBtn.Enabled);
+        }
+
+        [Test]
+        public void AttributesPage_NotificationTest()
+        {
+            // to open Attributes page 
+            AttributesPage_OpenPage();
+
+            var notificationIcon = driver.FindElement
+                (By.XPath("//*[@id=\"m_header_topbar\"]/div/ul/li[1]/a"));
+            Assert.IsTrue(notificationIcon.Displayed);
+            Assert.IsTrue(notificationIcon.Enabled);
+            Assert.AreEqual(notificationIcon.GetAttribute("href"),
+                "http://ec2-34-226-24-71.compute-1.amazonaws.com/App/Messages");
+            notificationIcon.Click();
+        }
+
+        [Test]
+        public void AttributesPage_PageTitleTest()
+        {
+            // to open Attributes page 
+            AttributesPage_OpenPage();
+
+            var title = driver.FindElement
+                (By.XPath("/html/body/div[1]/div/div[2]/div[1]/div/div/h1/span"));
+            Assert.IsTrue(title.Displayed);
+            Assert.IsTrue(title.Enabled);
+            Assert.AreEqual(title.Text, "Configuration - Attributes");
+        }
+
+        [Test]
+        public void AttributesPage_AssetClassTest()
+        {
+            // open attributes page
+            AttributesPage_OpenPage();
+
+            var assetClassLabel = driver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[1]/div/div/label"));
+            Assert.IsTrue(assetClassLabel.Displayed);
+            Assert.IsTrue(assetClassLabel.Enabled);
+            Assert.AreEqual(assetClassLabel.Text,"Asset Class");
+
+            var asstClassInput = driver.FindElement(By.Id("AssetClassIdChange"));
+            Assert.IsTrue(asstClassInput.Displayed);
+            Assert.IsTrue(asstClassInput.Enabled);
+            Assert.AreEqual(asstClassInput.GetAttribute("data-val-required"), "The ClassId field is required.");
+            var selectedAssetClass = new SelectElement(asstClassInput);
             selectedAssetClass.SelectByIndex(0);
-            assetClass.Click();
         }
 
         [Test]
-        public void ReviewerConfigurationAttributes_WhenSelectAssetSubClass_MustFilterAllAssetsBySubClass()
+        public void AttributesPage_AssetSubclass()
         {
-            // to click on configuration btn 
-            ReviewerConfigurationAttributes_WhenClickOnAttributesOption_MustOpenAttributesPage();
+            // open attributes page
+            AttributesPage_OpenPage();
 
-            var assetSubClass = driver.FindElement(By.Id("AssetSubClassDropDownChange"));
-            var selectedAssetSubClass = new SelectElement(assetSubClass);
-            selectedAssetSubClass.SelectByIndex(0);
-            assetSubClass.Click();
+            var assetSubClassLabel = driver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[2]/div/div/label"));
+            Assert.IsTrue(assetSubClassLabel.Displayed);
+            Assert.IsTrue(assetSubClassLabel.Enabled);
+            Assert.AreEqual(assetSubClassLabel.Text, "Asset Subclass");
+
+            var asstSubClassInput = driver.FindElement(By.Id("AssetSubClassDropDownChange"));
+            Assert.IsTrue(asstSubClassInput.Displayed);
+            Assert.IsTrue(asstSubClassInput.Enabled);
+            Assert.AreEqual(asstSubClassInput.GetAttribute("data-val-required"), "The SubClassId field is required.");
         }
 
         [Test]
-        public void ReviewerConfigurationAttributes_WhenSelectAssetType_MustFilterAllAssetsByType()
+        public void AttributesPage_AssetType()
         {
-            // to click on configuration btn 
-            ReviewerConfigurationAttributes_WhenClickOnAttributesOption_MustOpenAttributesPage();
+            // open attributes page
+            AttributesPage_OpenPage();
 
-            var assetClass = driver.FindElement(By.Id("AssetTypeDropDownChange"));
-            var selectedAssetClass = new SelectElement(assetClass);
-            selectedAssetClass.SelectByIndex(0);
-            assetClass.Click();
+            var assetSubClassLabel = driver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div[1]/div[3]/div/div/label"));
+            Assert.IsTrue(assetSubClassLabel.Displayed);
+            Assert.IsTrue(assetSubClassLabel.Enabled);
+            Assert.AreEqual(assetSubClassLabel.Text, "Asset Type");
+
+            var asstSubClassInput = driver.FindElement(By.Id("AssetTypeDropDownChange"));
+            Assert.IsTrue(asstSubClassInput.Displayed);
+            Assert.IsTrue(asstSubClassInput.Enabled);
+            Assert.AreEqual(asstSubClassInput.GetAttribute("data-val-required"), "The TypeId field is required.");
+        }
+
+        [Test]
+        public void AttributesPage_FooterCopyrightTest()
+        {
+            // to open Attributes page
+            AttributesPage_OpenPage();
+
+            var CopyRight = driver.FindElement(By.XPath("/html/body/footer/div/div/div[1]/span"));
+            Assert.IsTrue(CopyRight.Displayed);
+            Assert.IsTrue(CopyRight.Enabled);
+            Assert.AreEqual(CopyRight.Text, "2025 © CTDOT (Ver .)");
+        }
+
+        [Test]
+        public void AttributesPage_MinimizeToggle()
+        {
+            // to open Attributes page
+            AttributesPage_OpenPage();
+            var Toggle = driver.FindElement(By.Id("m_aside_left_minimize_toggle"));
+            Assert.IsTrue(Toggle.Displayed);
+            Assert.IsTrue(Toggle.Enabled);
+            Toggle.Click();
         }
     }
 }
